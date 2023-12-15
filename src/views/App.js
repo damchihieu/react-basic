b4db574430b58ad4a0e96e6b268cb3228d627bd7
damchihieu <damchihieu5@gmail.com>
2023-12-16 05:59:26 +0700

    video 4 Component

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.scss';
-
+import Mycomponent from './examble/Mycomponent';
+/**
+ * 
+ * co 2 loai component func va class su dung jsx
+ */
 function App() {
   return (
     <div className="App">
@@ -17,6 +21,7 @@ function App() {
         >
           Learn React
         </a>
+        <Mycomponent/>
       </header>
     </div>
   );
---
+++ src/views/examble/Mycomponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class Mycomponent extends React.Component{
+    render(){
+        return <div>Hello Mycompoment Dam chi Hieu</div>
+    }
+}
+
+export default Mycomponent;

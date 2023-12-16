0f9e2d87cc2100426acd5bc7a1b06438735d300d
damchihieu <damchihieu5@gmail.com>
2023-12-16 15:37:43 +0700

    #8 setstate

--- src/views/examble/Mycomponent.js
+++ src/views/examble/Mycomponent.js
@@ -1,25 +1,35 @@
 import React from 'react';
 class Mycomponent extends React.Component{
+    state = {
+        name: '',
+        channel: 'tu hoc sang tao'
+    }
     handleOnchangeName = (event) => {
+     //   console.log(event.target.value,'event target:',event.target,'event object:',event)
         this.setState({
-            name: event.target.value
+            name: event.target.value,
+            channel: 'abc'
         })
     }
-    state = {
-        name: 'Hieu',
-        channel: 'tu hoc sang tao'
-    }   
+    handleClickButon = () =>{
+        alert("click me")
+    }
+      
     render(){
-      //  let name = 'Hieu';
+        console.log('>>> call render:',this.state)
         return (
             <>
                 <div className='first'>
-                    <input value={this.state.name} type ="text"
+                    <input value={this.state.name} type ="text" 
                         onChange = {(event) => this.handleOnchangeName(event)}/>
                      Hello my name: {this.state.name}
                  </div>
                  <div className='second'>
                     kenh channal: {this.state.channel}
+                 </div>
+                 <div className='third'>
+                    <button onClick={() =>this.handleClickButon()}>click me</button>
+
                  </div>
             </>
             

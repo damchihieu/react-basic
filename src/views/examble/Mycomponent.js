c323c4bed4d5f56910023de5ce52b53e17ab9e94
damchihieu <damchihieu5@gmail.com>
2023-12-16 07:10:35 +0700

    #5 State

--- src/views/examble/Mycomponent.js
+++ src/views/examble/Mycomponent.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
-
 class Mycomponent extends React.Component{
+    handleOnchangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+    state = {
+        name: 'Hieu',
+        channel: 'tu hoc sang tao'
+    }   
     render(){
-        return <div>Hello Mycompoment Dam chi Hieu</div>
+      //  let name = 'Hieu';
+        return (
+            <>
+                <div className='first'>
+                    <input value={this.state.name} type ="text"
+                        onChange = {(event) => this.handleOnchangeName(event)}/>
+                     Hello my name: {this.state.name}
+                 </div>
+                 <div className='second'>
+                    kenh channal: {this.state.channel}
+                 </div>
+            </>
+            
+        )
     }
 }
 

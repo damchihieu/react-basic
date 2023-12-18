4de74553f289d0cf2f14c6e018d20a8f02e024f1
damchihieu <damchihieu5@gmail.com>
2023-12-18 14:33:49 +0700

    video #13 function conponent

--- src/views/examble/ChildComponent.js
+++ src/views/examble/ChildComponent.js
@@ -1,35 +1,40 @@
 import React from 'react';
-class ChildComponent extends React.Component{
-    state = {
-        firstname:'',
-        lastname:''
-    }
-    handleonchangefirstname = (event) =>{
-        this.setState({
-            firstname: event.target.value
-        })
-    }
-    handleonchangelastname = (event)=>{
-        this.setState({
-            lastname: event.target.value
-        })
-    }
-    handleOnClick = (event) =>{
-        event.preventDefault()
-        console.log(">>> check data input",this.state)
-    }
+//class ChildComponent extends React.Component{
+    
       
-    render(){
-        console.log('>>> check props:',this.props)
-        // let name = this.props.name;
-        // let age = this.props.age;
+    // render(){
+    //     console.log('>>> check props:',this.props)
+    //     // let name = this.props.name;
+    //     // let age = this.props.age;
 
-        let {name,age,address,arr} = this.props;
+    //     let {name,age,address,arr} = this.props;
+    //     let a = '';
+    //     return (
+    //         <>
+               
+    //            <div> Child Component name : {name} - {age} - {address}</div>
+    //            <div className='job-list'>
+    //             {
+    //               a=  arr.map((item,index) =>{
+    //                     return (
+    //                         <div key={item.id}>
+    //                             {item.title}- {item.salary}
+    //                         </div>
+    //                     )
+    //                 })
+    //             }    
+    //            </div>
+    //             {console.log('>>>> check map array:',a)}
+    //         </>       
+    //     )
+    // }
+//}
+const ChildComponent = (props) =>{
+        let {arr} = props;
         let a = '';
         return (
             <>
                
-               <div> Child Component name : {name} - {age} - {address}</div>
                <div className='job-list'>
                 {
                   a=  arr.map((item,index) =>{
@@ -44,7 +49,5 @@ class ChildComponent extends React.Component{
                 {console.log('>>>> check map array:',a)}
             </>       
         )
-    }
 }
-
 export default ChildComponent;
